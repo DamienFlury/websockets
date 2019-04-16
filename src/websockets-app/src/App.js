d5d52bd014fdb3716cd41890365e6860cf63c6f8
Damien Flury <damien.flury@gmail.com>
2019-04-16 16:57:48 +0200

    Implement chat

--- src/websockets-app/src/App.js
+++ src/websockets-app/src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
+import { HubConnectionBuilder } from '@aspnet/signalr';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+const connection = new HubConnectionBuilder().withUrl("https://localhost:5001/chatHub").build();
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [text, setText] = useState('');
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    connection.on('ReceiveMessage', (user, message) => {
+      setMessage(message);
+      setUser(user);
+    });
+    connection.start().then(() => console.log('Running...')).catch(console.log);
+  }, [])
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    connection.invoke('SendMessage', user, text).catch(console.log);
   }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        <button type="submit">Send</button>
+        <div>
+          <span>User: {user}</span>
+          <span>Message: {message}</span>
+        </div>
+      </form>
+    </div>
+  );
 }
+
 
 export default App;
--- src/websockets-server/WebsocketsServer.Api/Startup.cs
+++ src/websockets-server/WebsocketsServer.Api/Startup.cs
@@ -12,42 +12,40 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using WebsocketsServer.Api.Hubs;
 
-namespace WebsocketsServer.Api
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace WebsocketsServer.Api {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddSignalR();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
+            services.AddSignalR ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts ();
             }
 
-            app.UseHttpsRedirection();
-            app.UseSignalR(routes => {
-                routes.MapHub<ChatHub>("/chatHub");
+            app.UseHttpsRedirection ();
+            app.UseCors (builder => {
+                builder.WithOrigins ("http://localhost:3000", "http://localhost:3001")
+                    .AllowAnyHeader ()
+                    .WithMethods ("GET", "POST")
+                    .AllowCredentials ();
             });
-            app.UseMvc();
+            app.UseSignalR (routes => {
+                routes.MapHub<ChatHub> ("/chatHub");
+            });
+            app.UseMvc ();
         }
     }
 }
